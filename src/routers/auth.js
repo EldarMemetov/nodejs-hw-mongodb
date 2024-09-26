e813c02927ee91329b9ad21e9bbb4a10e6e03a99
EldarMemetov <eldarm9347@gmail.com>
2024-09-26 19:57:33 +0200

    update

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,10 +1,8 @@
+// /routers/auth.js
 import { Router } from 'express';
-
 import validateBody from '../utils/validateBody.js';
 import ctrlWrapper from '../utils/ctrlWrapper.js';
-
 import * as authControllers from '../controllers/auth.js';
-
 import { userSigninSchema, userSignupSchema } from '../validation/users.js';
 
 const authRouter = Router();
@@ -22,7 +20,6 @@ authRouter.post(
 );
 
 authRouter.post('/refresh', ctrlWrapper(authControllers.refreshController));
-
-authRouter.post('/signup', ctrlWrapper(authControllers.signoutController));
+authRouter.post('/signout', ctrlWrapper(authControllers.signoutController));
 
 export default authRouter;
